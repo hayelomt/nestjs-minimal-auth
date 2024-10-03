8ec18d7f0b3031da3dacafd8fb4f6d09e6e0c005
Hayelom Tizazu <hayelomtizazu19@gmail.com>
2024-10-03 17:39:30 +0300

    move callback to middleware

--- src/auth.ts
+++ src/auth.ts
@@ -1,10 +1,62 @@
-import NextAuth from 'next-auth';
+import NextAuth, { type DefaultSession } from 'next-auth';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { JWT } from 'next-auth/jwt';
 import { PrismaAdapter } from '@auth/prisma-adapter';
+
 import authConfig from './auth.config';
 import db from './lib/db';
+import { getUserById } from './services/auth-service';
+import { UserRole } from '@prisma/client';
+
+declare module 'next-auth' {
+  /**
+   * Returned by `auth`, `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session {
+    user: {
+      /** The user's postal address. */
+      role?: UserRole;
+      /**
+       * By default, TypeScript merges new interface properties and overwrites existing ones.
+       * In this case, the default session user properties will be overwritten,
+       * with the new ones defined above. To keep the default session user properties,
+       * you need to add them back into the newly declared interface.
+       */
+    } & DefaultSession['user'];
+  }
+}
+declare module 'next-auth/jwt' {
+  /** Returned by the `jwt` callback and `auth`, when using JWT sessions */
+  interface JWT {
+    role?: UserRole;
+  }
+}
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   ...authConfig,
   adapter: PrismaAdapter(db),
   session: { strategy: 'jwt' },
+  callbacks: {
+    jwt: async ({ token }) => {
+      if (!token.sub) return token;
+
+      const user = await getUserById(token.sub);
+
+      if (user) {
+        token.role = user.role;
+      }
+
+      return token;
+    },
+    session: async ({ token, session }) => {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+      if (token.role && session.user) {
+        session.user.role = token.role;
+      }
+
+      return session;
+    },
+  },
 });
